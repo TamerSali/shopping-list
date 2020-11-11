27b91ca0333e1e7116b9e91f34eb5afb766c635a
Tamer Sali <tamerrsali@gmail.com>
2020-11-11 14:27:44 +0200

    add item to list

--- src/components/shopping-form.js
+++ src/components/shopping-form.js
@@ -1,17 +1,44 @@
 /**
  * External dependencies.
  */
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function ShoppingForm() {
+export default function ShoppingForm (props) {
+	const [value, setValue] = useState("");
+
+	const getUniqueID = () => {
+		return Date.now().toString(36) + Math.random().toString(36).substr(2);
+	}
+
+	const handleInputChange = e => {
+		setValue(e.target.value)
+	}
+
+	const handleUserSubmit = e => {
+		e.preventDefault();
+
+		if (value.trim() === "") {
+			return;
+		}
+
+		props.onSubmit({
+			id: getUniqueID(),
+			text: value,
+			complete: false,
+		});
+		setValue("");
+	}
+
 	return(
 		<div className="shopping-form">
-			<form>
+			<form onSubmit={handleUserSubmit}>
 				<i
 					className="fal fa-2x fa-check-circle"
 				/>
 				<input
 					type="text"
+					value={value}
+					onChange={handleInputChange}
 					placeholder="What I need ot buy today"
 				/>
 			</form>
