12fb4b1e45a0f10470901bcd25a5143c8836e039
Tamer Sali <tamerrsali@gmail.com>
2020-11-11 14:29:04 +0200

    handle adding item and render state

--- src/components/shopping-list.js
+++ src/components/shopping-list.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies.
  */
-import React from 'react'
+import React, { useState } from 'react'
 /**
  * Internal dependencies.
  */
@@ -9,11 +9,22 @@ import ShoppingForm from './shopping-form'
 import ShoppingItem from './shopping-item'
 
 export default function ShoppingList() {
+	const [stateItems, setItems] = useState([]);
+
+	const handleItemAdd = item => {
+		setItems([...stateItems,item]);
+	}
 
 	return(
 		<div className="shopping-list">
-			<ShoppingForm/>
-			<ShoppingItem/>
+			<ShoppingForm onSubmit={handleItemAdd}/>
+			{!stateItems.length && (
+				<div className="empty-list">No Items Here</div>
+			)}
+
+			{stateItems.map(item => (
+				<ShoppingItem key={item.id} item={item} />
+			))}
 		</div>
 	)
 }
