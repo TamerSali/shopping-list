5c874b66f47099d96ef6935e16f0987d01932e8e
Tamer Sali <tamerrsali@gmail.com>
2020-11-11 13:56:12 +0200

    render shopping list component

--- src/app.js
+++ src/app.js
@@ -5,11 +5,17 @@ import React, { Fragment } from 'react';
 /**
  * Internal dependencies.
  */
+import ShoppingList from './components/shopping-list'
+
 function App() {
 
 	return (
+		<Fragment>
+			<h1>My Shopping List</h1>
 			<div className="app">
+				<ShoppingList/>
 			</div>
+		</Fragment>
 	)
 
 }
