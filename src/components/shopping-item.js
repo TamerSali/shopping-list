cc1237eaa38ee1d94a9a563db0ca2b43419eb09a
Tamer Sali <tamerrsali@gmail.com>
2020-11-11 14:28:17 +0200

    display items

--- src/components/shopping-item.js
+++ src/components/shopping-item.js
@@ -3,11 +3,13 @@
  */
 import React from 'react'
 
-export default function ShoppingItem() {
+export default function ShoppingItem (props) {
+	const { item } = props;
+
 	return(
 
 		<div className="shopping-item">
-
+			{item.text}
 		</div>
 	)
 }
