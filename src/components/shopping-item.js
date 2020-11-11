c1b30bbac9b78f8aa2ea7b3b7753f02394d0f88c
Tamer Sali <tamerrsali@gmail.com>
2020-11-11 14:40:14 +0200

    added toggle complete and delete item methods

--- src/components/shopping-item.js
+++ src/components/shopping-item.js
@@ -4,12 +4,22 @@
 import React from 'react'
 
 export default function ShoppingItem (props) {
-	const { item } = props;
+	const { item, onToggle, onDelete } = props;
+
 
 	return(
 
 		<div className="shopping-item">
-			{item.text}
+		<i
+			className="fal fa-2x fa-check-circle"
+			style={{color: item.complete && "green"}}
+			onClick={ () => onToggle(item.id)}
+		/>
+		{item.text}
+		<i
+			className="fal fa-lg fa-trash delete-item"
+			onClick={ () => onDelete(item.id)}
+		/>
 		</div>
 	)
 }
--- src/components/shopping-list.js
+++ src/components/shopping-list.js
@@ -14,17 +14,36 @@ export default function ShoppingList() {
 	const handleItemAdd = item => {
 		setItems([...stateItems,item]);
 	}
+	const handleToggleComplete = id => {
+		const lastStateOfItems = stateItems.map(item => {
+			return item.id === id
+				? {...item, complete: ! item.complete}
+				: item;
+		});
+		setItems(lastStateOfItems);
+	}
+	const handleDelete = id => {
+		const lastStateOfItems = stateItems.filter(item => item.id !== id);
+		setItems(lastStateOfItems);
+	}
 
 	return(
 		<div className="shopping-list">
 			<ShoppingForm onSubmit={handleItemAdd}/>
+
 			{!stateItems.length && (
 				<div className="empty-list">No Items Here</div>
 			)}
 
 			{stateItems.map(item => (
-				<ShoppingItem key={item.id} item={item} />
+				<ShoppingItem
+					key={item.id}
+					item={item}
+					onToggle={handleToggleComplete}
+					onDelete={handleDelete}
+				/>
 			))}
+
 		</div>
 	)
 }
